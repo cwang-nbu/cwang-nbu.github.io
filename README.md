98ac2b67a134fad55d7fa2ebf469dbbd48d650c6
翀 <chong@chongsMacBook-Pro.local>
2026-06-30 19:26:20 +0800

    Update generated site

--- README.md
+++
@@ -1,3 +0,0 @@
-# cwang-nbu.github.io
-
-My homepage with introduction of the members in the research team.
--- js/wowchemy-init.min.098c85d525c4f5a050a4be02fe23fb93.js
+++
@@ -1,6 +0,0 @@
-/*! Wowchemy v5.4.0 | https://wowchemy.com/ */
-/*! Copyright 2016-present George Cushen (https://georgecushen.com/) */
-/*! License: https://github.com/wowchemy/wowchemy-hugo-themes/blob/main/LICENSE.md */
-
-;
-(()=>{(()=>{function e(){const e=localStorage.getItem("siteTheme")||"auto";return e==="light"?0:e==="dark"?1:2}function t(){return document.body.classList.contains("dark")}function n(){return{isDarkTheme:t(),themeMode:e()}}var s="minimal",o=!1;window.wc={darkLightEnabled:s,isSiteThemeDark:o},window.netlifyIdentity&&window.netlifyIdentity.on("init",e=>{e||window.netlifyIdentity.on("login",()=>{document.location.href="/admin/"})}),n(),window.PlotlyConfig={MathJaxConfig:"local"}})()})()
